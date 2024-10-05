b301f4cbf4df23d7b25b624041c992ba8e2c1e05
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-10-05 17:42:13 -0300

    Classe de ligação OrderItem

---
+++ src/main/java/com/brunov/sistemapedidos/entity/pk/OrderItemPK.java
@@ -0,0 +1,35 @@
+package com.brunov.sistemapedidos.entity.pk;
+
+import com.brunov.sistemapedidos.entity.Order;
+import com.brunov.sistemapedidos.entity.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter @Setter
+@Embeddable
+public class OrderItemPK  {
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK that)) return false;
+        return Objects.equals(getOrder(), that.getOrder()) && Objects.equals(getProduct(), that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getOrder(), getProduct());
+    }
+}
